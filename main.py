7afcdcdf0bfa842704b069743b8961cc1bffd7b2
chloeboireaudevier <cbd65@hotmail.fr>
2025-03-23 15:53:31 +0100

    first push

---
+++ main.py
@@ -0,0 +1,20 @@
+#Constantes
+
+global GREEN 
+GREEN = (16,250,54)
+global ORANGE
+ORANGE = (250,150,15)
+global RED 
+RED = (250,16,16)
+global BLUE 
+BLUE = (16,63,250)
+global YELLOW 
+YELLOW = (241,250,16)
+global WHITE 
+WHITE = (255,255,255)
+
+colors = [GREEN,ORANGE,RED,BLUE,YELLOW,WHITE]
+
+stickers = {
+
+}
